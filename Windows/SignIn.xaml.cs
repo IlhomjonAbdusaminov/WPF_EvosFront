e5f26fc1bab15ef14d8dcb7cd8f2fdd11a01e291
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-07 16:50:42 +0500

    There are still drawbacks

--- Windows/SignIn.xaml.cs
+++ Windows/SignIn.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WPFEvosFront.Windows;
 
-namespace WPFEvosFront
+namespace WPFEvosFront.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SignIn.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SignIn : Window
     {
-        public MainWindow()
+        public SignIn()
         {
             InitializeComponent();
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            SignIn signIn = new SignIn();
-            signIn.Show();
         }
     }
 }
