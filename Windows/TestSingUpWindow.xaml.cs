262a2418de9da8968371f100934729ee8c94ff34
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-09 21:39:38 +0500

    Bug Fixed

--- Windows/TestSingUpWindow.xaml.cs
+++ Windows/TestSingUpWindow.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace WPFEvosFront.Windows
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for TestSingUpWindow.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class TestSingUpWindow : Window
     {
-        public Window1()
+        public TestSingUpWindow()
         {
             InitializeComponent();
         }
